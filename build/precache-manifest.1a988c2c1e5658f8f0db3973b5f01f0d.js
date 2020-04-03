977d346d7163b4a5c21ba5d8c9a29eeb2e164e8e
Brennan <brennancodes@gmail.com>
2020-04-03 11:23:00 -0500

    Initializing new home for portfolio after updating resume, testimonials, and cleaning up

---
+++ build/precache-manifest.1a988c2c1e5658f8f0db3973b5f01f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a68faf59e619de8672e10bf3e7ac73c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e5c24706bcfa24970b",
+    "url": "/static/css/main.30fff87b.chunk.css"
+  },
+  {
+    "revision": "a209d5fcc3dc3f6ed224",
+    "url": "/static/js/2.04fd2397.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/static/js/2.04fd2397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e5c24706bcfa24970b",
+    "url": "/static/js/main.5f418a89.chunk.js"
+  },
+  {
+    "revision": "1196d83f838602b6fc51",
+    "url": "/static/js/runtime-main.f1053bf2.js"
+  }
+]);
